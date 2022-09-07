be273c2343199351ac475331a89e9bffa3c05bfe
Anwar Ali <anwar08ali@gmail.com>
2022-09-07 14:43:05 +0530

    Unit test case added for App rendering

--- image-search-app/src/App.test.js
+++ image-search-app/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import ImageContext from "./components/Context/ImageContext";
+test("renders learn react link", () => {
+  render(
+    <ImageContext>
+      <App />
+    </ImageContext>
+  );
+  const linkElement = screen.getByText(/Image Search App/i);
   expect(linkElement).toBeInTheDocument();
 });
